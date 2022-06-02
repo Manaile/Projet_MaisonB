1e4a91b3727a414da681251ef410111c3fc2f5f9
Manaile Benazzi <b.manaile@gmail.com>
2022-06-02 23:26:30 +0200

    responsive ok

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -1,8 +1,35 @@
-$('.reviews').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 50000000,
-      dots: false,
-      arrows:true
-  });
+
+if(window.innerWidth > 1000){
+    $('.reviews').slick({
+        
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 50000000,
+            dots: false,
+            arrows:true,
+        
+    })
+}else if( window.innerWidth > 800 && window.innerWidth < 999){
+    $('.reviews').slick({
+    
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 50000000,
+        dots: false,
+        arrows:true,
+      
+  })
+  }else if( window.innerWidth < 799){
+    $('.reviews').slick({
+    
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 50000000,
+        dots: false,
+        arrows:true,
+      
+  })
+  };
